7c40f98c5a4f9db27254cb974eecd550b951d0b2
Lorna Sherwood <lornasherwood@gmail.com>
2016-12-16 10:43:11 +0000

    create db tables

---
+++ db/pet_shelter.sql
@@ -0,0 +1,26 @@
+DROP TABLE animals;
+DROP TABLE owners;
+
+CREATE TABLE owners
+(
+  id SERIAL8 primary key,
+  first_name VARCHAR(255),
+  last_name VARCHAR(255),
+  address_1 VARCHAR(255),
+  address_2 VARCHAR(255),
+  address_3 VARCHAR(255)
+);
+
+CREATE TABLE animals
+(
+  id SERIAL8 primary key,
+  name VARCHAR(255),
+  breed VARCHAR(255),
+  admission_date DATE,
+  status VARCHAR(255),
+  photo VARCHAR(255),
+  owner_id SERIAL8 references owners(id) 
+);
+
+
+
